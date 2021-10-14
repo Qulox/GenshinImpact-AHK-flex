30447bfc9117e0a865658b71048e8b7dfe14efab
Kramar1337 <73392036+Kramar1337@users.noreply.github.com>
2021-10-14 10:17:57 +0300

    fix

--- Genshin AHK/data/pip instal.bat
+++ Genshin AHK/data/pip instal.bat
@@ -1,5 +1,5 @@
-pip install asyncio
-pip install mido
-pip install keyboard
-pip install pynput
+py -m pip install asyncio
+py -m pip install mido
+py -m pip install keyboard
+py -m pip install pynput
 pause
